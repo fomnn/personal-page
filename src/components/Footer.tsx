e39c87a2677117086d6f6f4d7988cd87dc14f7d5
fomnn <fathurrahmannotoy@gmail.com>
2024-10-01 15:06:51 +0700

    add react and vite logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
+
+import DeviconReact from '~icons/devicon/react'
+import LogosVitejs from '~icons/logos/vitejs'
 export default function Footer() {
     return (
         <footer className="w-full py-3">
-            <p className="w-fit mx-auto text-sm">© 2024 Fathur Rahman Nur.</p>
+          <span className='flex items-center gap-2 justify-center mb-4 text-sm'>Built with React <DeviconReact /> and Vite <LogosVitejs /></span>
+            <p className="w-fit mx-auto text-sm">2024 | Fathur Rahman Nur.</p>
         </footer>
     )
 }
